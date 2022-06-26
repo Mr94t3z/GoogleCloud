4dab39e7827f8484f9b34996ff7f933783735076
Muhamad Taopik <mr.94t3z@gmail.com>
2022-06-26 18:03:18 +0700

    🌩️

---
+++ hapi-web-server/server.js
@@ -0,0 +1,27 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+ 
+ 
+const init = async () => {
+    const server = Hapi.server({
+        port: 9999,
+        host: 'localhost',
+    });
+ 
+    server.route(routes);
+ 
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+init();
+
+
+// curl -X GET http://localhost:9999
+// // output: Homepage
+// curl -X GET http://localhost:9999/about
+// // output: About page
+// curl -X GET http://localhost:9999/test
+// // output: Halaman tidak ditemukan
+// curl -X POST http://localhost:9999
+// // output: Halaman tidak dapat diakses dengan method tersebut
