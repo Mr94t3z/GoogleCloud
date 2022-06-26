07ab76a5f4ac9d93a177b3bb489af6327d3ca21e
Muhamad Taopik <mr.94t3z@gmail.com>
2022-06-26 19:54:41 +0700

    🌩️

--- notes-app-back-end/src/server.js
+++ notes-app-back-end/src/server.js
@@ -5,6 +5,11 @@ const init = async () => {
   const server = Hapi.server({
     port: 9999,
     host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
 
   server.route(routes);
